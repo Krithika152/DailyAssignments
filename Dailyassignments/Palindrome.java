7a3988585bedab3e459ce16a7db402b5192be559
Krithika152 <122201686+Krithika152@users.noreply.github.com>
2023-01-18 22:48:18 +0530

    Add files via upload

---
+++ Dailyassignments/Palindrome.java
@@ -0,0 +1,25 @@
+package Dailyassignments;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		String s= "madam";
+		String rev= "";
+		
+ char[] s1 = s.toCharArray();		
+ for (int i = s1.length-1; i>=0; i--) {
+	 rev=rev+s1[i];
+ }
+	 
+	 if(s.equalsIgnoreCase(rev)) {
+		 System.out.print("It is palindrome");
+		 
+	 }
+	 else {
+		 System.out.print("It is not a palindrome");
+	 }
+	
+}
+
+	}
+
