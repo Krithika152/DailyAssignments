eaeefdb65f9eff23370497f386b6f80a477f2ecb
Krithika152 <122201686+Krithika152@users.noreply.github.com>
2023-01-18 22:47:57 +0530

    Add files via upload

---
+++ Dailyassignments/Findsquareroot.java
@@ -0,0 +1,12 @@
+package Dailyassignments;
+
+public class Findsquareroot {
+
+	public static int main(int number) {
+		double num=16/2;
+		double temp = num;
+		num=((num/temp)+temp)/2;
+		return number;
+	}
+
+}
